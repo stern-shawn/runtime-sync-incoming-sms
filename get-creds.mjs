2cb6bde829f7fd2365e7a07e93ee2e0fa2a54850
Shawn Stern <sstern@twilio.com>
2022-01-27 10:28:01 -0800

    fix: add AUTH_TOKEN env var to fix twilio client init

--- get-creds.mjs
+++ get-creds.mjs
@@ -3,7 +3,10 @@
 $.verbose = false;
 
 const accountSid = await question(
-  `${chalk.cyan.underline.bold('What is your Twilio Account SID?:')} `
+  `${chalk.cyan.underline.bold('What is your Account SID?:')} `
+);
+const authToken = await question(
+  `${chalk.cyan.underline.bold('What is your Auth token?:')} `
 );
 const apiKey = await question(
   `${chalk.cyan.underline.bold('What is your API Key SID?:')} `
@@ -12,7 +15,8 @@ const apiSecret = await question(
   `${chalk.cyan.underline.bold('What is your API Key Secret?:')} `
 );
 
-await $`printf "ACCOUNT_SID=${accountSid}\nTWILIO_API_KEY=${apiKey}\nTWILIO_API_SECRET=${apiSecret}\nTWILIO_SYNC_SERVICE_SID=\nSYNC_LIST_NAME=" >> .env`;
+await $`printf "ACCOUNT_SID=${accountSid}\nAUTH_TOKEN=${authToken}\nTWILIO_API_KEY=${apiKey}\nTWILIO_API_SECRET=${apiSecret}\nTWILIO_SYNC_SERVICE_SID=\nSYNC_LIST_NAME=" >> .env`;
 await $`export ACCOUNT_SID=${accountSid}`;
+await $`export AUTH_TOKEN=${authToken}`;
 await $`export TWILIO_API_KEY=${apiKey}`;
 await $`export TWILIO_API_SECRET=${apiSecret}`;
