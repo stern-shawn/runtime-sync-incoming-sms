c0591c94d49022b87c944b360f31d3220c966231
Shawn Stern <sstern@twilio.com>
2022-01-26 21:59:16 -0800

    fix: export env vars so they can be picked up by CLI

--- get-creds.mjs
+++ get-creds.mjs
@@ -12,4 +12,7 @@ const apiSecret = await question(
   `${chalk.cyan.underline.bold('What is your API Key Secret?:')} `
 );
 
-await $`printf "TWILIO_ACCOUNT_SID=${accountSid}\nTWILIO_API_KEY=${apiKey}\nTWILIO_API_SECRET=${apiSecret}\nTWILIO_SYNC_SERVICE_SID=\nSYNC_LIST_NAME=" >> .env`;
+await $`printf "ACCOUNT_SID=${accountSid}\nTWILIO_API_KEY=${apiKey}\nTWILIO_API_SECRET=${apiSecret}\nTWILIO_SYNC_SERVICE_SID=\nSYNC_LIST_NAME=" >> .env`;
+await $`export ACCOUNT_SID=${accountSid}`;
+await $`export TWILIO_API_KEY=${apiKey}`;
+await $`export TWILIO_API_SECRET=${apiSecret}`;
